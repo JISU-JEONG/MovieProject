5e2990a462d43d2be0083df0e440feef583bdb2f
Yeon-Yongheum <yeon931@naver.com>
2019-11-25 18:30:44 +0900

    index 최종

--- movies/views.py
+++ movies/views.py
@@ -4,6 +4,9 @@ from django.contrib import messages
 from .models import Movie,Review, Score, Genre
 from .forms import ReviewForm, ScoreForm
 
+
+flag = 0
+movie_list_1 = []
 # simil 함수 유저가 준 점수 평균
 def simil(u1, u2):
     movies = movies.objects.all()
@@ -19,10 +22,9 @@ def simil(u1, u2):
         return S/((S_u1**0.5)*(S_u2**0.5))
     except:
         return 0
-
 # rating 함수
 def rating(movie, user, cmp_users):
-    S,abs_S = 0,0 
+    S,abs_S = 0,0
     for cmp_user in cmp_users:
         if cmp_user in movies.rate_user.all(): continue
         result = simil(user,cmp_user)
@@ -33,7 +35,6 @@ def rating(movie, user, cmp_users):
         return user.rate + S/abs_S
     except:
         return user.rate
-
 def solve(user):
     users = User.objects.all()
     movies = Movie.objects.all()
@@ -44,23 +45,67 @@ def solve(user):
         cmp_users.append((simil(user, tmp), tmp))
     cmp_users.sort(reverse=True)
     for i in range(5):
-        tmp.append(cmp_users[i][1])
+        tmps.append(cmp_users[i][1])
     cmp_movies = []
     for movie in movies:
         if movie in user.rate_movies.all(): continue
-        cmp_movies.append((rating(movie,user,tmp),movie))
+        cmp_movies.append((rating(movie,user,tmps),movie))
     cmp_movies.sort(reverse=True)
     recommend = []
     for i in range(5):
         recommend.append(cmp_movies[i][1])
     return recommend
-
 # Create your views here.
 def index(request):
     movies = Movie.objects.all()
-    user = request.user 
+    user = request.user
     genres = Genre.objects.all()
     recommend = []
+    global movie_list_1
+    global flag
+    if flag:
+        movie_list = movie_list_1
+    elif not flag:
+        movie_list = [[] for _ in range(18)]
+        for movie in movies:
+            if movie.genres.all().filter(pk=28) and len(movie_list[0])<20:
+                movie_list[0].append(movie)
+            if movie.genres.all().filter(pk=12) and len(movie_list[1])<20:
+                movie_list[1].append(movie)
+            if movie.genres.all().filter(pk=16) and len(movie_list[2])<20:
+                movie_list[2].append(movie)
+            if movie.genres.all().filter(pk=35) and len(movie_list[3])<20:
+                movie_list[3].append(movie)
+            if movie.genres.all().filter(pk=80) and len(movie_list[4])<20:
+                movie_list[4].append(movie)
+            if movie.genres.all().filter(pk=99) and len(movie_list[5])<20:
+                movie_list[5].append(movie)
+            if movie.genres.all().filter(pk=18) and len(movie_list[6])<20:
+                movie_list[6].append(movie)
+            if movie.genres.all().filter(pk=10751) and len(movie_list[7])<20:
+                movie_list[7].append(movie)
+            if movie.genres.all().filter(pk=14) and len(movie_list[8])<20:
+                movie_list[8].append(movie)
+            if movie.genres.all().filter(pk=36) and len(movie_list[9])<20:
+                movie_list[9].append(movie)
+            if movie.genres.all().filter(pk=27) and len(movie_list[10])<20:
+                movie_list[10].append(movie)
+            if movie.genres.all().filter(pk=10402) and len(movie_list[11])<20:
+                movie_list[11].append(movie)
+            if movie.genres.all().filter(pk=9648) and len(movie_list[12])<20:
+                movie_list[12].append(movie)
+            if movie.genres.all().filter(pk=10749) and len(movie_list[13])<20:
+                movie_list[13].append(movie)
+            if movie.genres.all().filter(pk=878) and len(movie_list[14])<20:
+                movie_list[14].append(movie)
+            if movie.genres.all().filter(pk=53) and len(movie_list[15])<20:
+                movie_list[15].append(movie)
+            if movie.genres.all().filter(pk=10752) and len(movie_list[16])<20:
+                movie_list[16].append(movie)
+            if movie.genres.all().filter(pk=37) and len(movie_list[17])<20:
+                movie_list[17].append(movie)
+            flag = 1
+            movie_list_1 = movie_list
     if request.user.is_authenticated and len(user.rate_movies.all())>10:
         recommend = solve(user)
     else:
@@ -69,7 +114,7 @@ def index(request):
             S,cnt = 0,0
             for tmp in movie.rate_users.all():
                 cnt+=1
-                S+=Score.objects.filter(movie_id = movie.id, user_id = request.user.id)[0].score
+                S+=Score.objects.filter(movie_id = movie.id, user_id = tmp.id)[0].score
             try:
                 recommend.append((S/cnt,movie))
             except:
@@ -82,10 +127,26 @@ def index(request):
     context = {
         'movies' : movies,
         'recommend' : recommend,
-        'genres' : genres
+        'genres' : genres,
+        'actions' : movie_list[0],
+        'advs' : movie_list[1],
+        'anis' : movie_list[2],
+        'comis' : movie_list[3],
+        'guls' : movie_list[4],
+        'docus' : movie_list[5],
+        'dramas' : movie_list[6],
+        'fams' : movie_list[7],
+        'fans' : movie_list[8],
+        'his' : movie_list[9],
+        'afrs' : movie_list[10],
+        'musics' : movie_list[11],
+        'mis' : movie_list[12],
+        'romans' : movie_list[13],
+        'SFs' : movie_list[14],
+        'thrils' : movie_list[15],
+        'wars' : movie_list[16],
+        'wests' : movie_list[17],
     }
-    print(movies)
-    print(recommend)
     return render(request,"movies/index.html",context)
 
 def detail(request, movie_id):
